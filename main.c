d0fa33a3601d9ba15a5ddc4687dd4df68c88cca8
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2023-11-12 22:36:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include "pj.h"
 
+
+
+int main() {
+    Task tarefas[MAX_TASKS];
+    int numTarefas = 0;
+
+    carregarTarefas(tarefas, &numTarefas);
+
+    int opcao;
+    do {
+        printf("\n=== Menu ===\n");
+        printf("1. Cadastrar tarefa\n");
+        printf("2. Listar tarefas\n");
+        printf("3. Deletar tarefa\n");
+        printf("4. Salvar tarefas\n");
+        printf("5. Sair\n");
+        printf("Digite sua escolha: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarTarefa(tarefas, &numTarefas);
+                break;
+            case 2:
+                listarTarefas(tarefas, numTarefas);
+                break;
+            case 3:
+                deletarTarefa(tarefas, &numTarefas);
+                break;
+            case 4:
+                salvarTarefas(tarefas, numTarefas);
+                break;
+            case 5:
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 5);
+
+    return 0;
+}
